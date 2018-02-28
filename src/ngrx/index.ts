071ceb992a7a890d78fa76e4c8d9ac7e7ea632e0
Robert Taylor <roberttaylor@tengroup.com>
2018-02-28 12:20:42 +0000

    refactor(schematics): update template filenames to use variables

--- src/ngrx/index.ts
+++ src/ngrx/index.ts
@@ -2,8 +2,6 @@ import { apply, chain, mergeWith, template, url } from '@angular-devkit/schemati
 import { camelize, capitalize, classify, dasherize } from '@angular-devkit/core/src/utils/strings';
 import { NgrxOptions } from './schema';
 
-const stringUtils = { dasherize };
-
 export default function (options: NgrxOptions) {
 
     const className = classify(options.name);
@@ -14,7 +12,6 @@ export default function (options: NgrxOptions) {
     const templateSource = apply(url('./files'), [
         template({
             ...options,
-            ...stringUtils,
             className,
             capitalizedName,
             kebabCaseName,

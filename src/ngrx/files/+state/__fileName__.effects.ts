95bd41aa31cd9aacc4be9984f2ed5573ddfc350c
robt1019 <roberttaylor@tengroup.com>
2018-05-21 10:40:44 +0100

    feat(ngrx): update effects template to match new architecture

--- src/ngrx/files/+state/__fileName__.effects.ts
+++ src/ngrx/files/+state/__fileName__.effects.ts
@@ -2,20 +2,33 @@ import { Injectable } from '@angular/core';
 import { Effect, Actions } from '@ngrx/effects';
 import { DataPersistence } from '@nrwl/nx';
 
-import { <%= className %>State } from './<%= fileName %>.reducer';
-import { LoadDataAction, LoadDataSuccessAction, <%= className %>ActionTypes } from './<%= fileName %>.actions';
+import {
+  Load<%= className %>Action,
+  Load<%= className %>SuccessAction,
+  <%= className %>ActionTypes
+} from './<%= fileName %>.actions';
+import { <%= className %>FeatureStore } from './<%= fileName %>.reducer'
 
 @Injectable()
 export class <%= className %>Effects {
-@Effect() loadData = this.dataPersistence.fetch(<%= className %>ActionTypes.LOAD_DATA, {
-        run: (action: LoadDataAction, state: <%= className %>State) => {
-        return new LoadDataSuccessAction('Test Data');
-    },
+  @Effect()
+  public load<%= className %> = this.dataPersistence.fetch<Load<%= className %>Action>(
+    <%= className %>ActionTypes.LOAD_<%= constName %>,
+      {
+      run: (action: Load<%= className %>Action, state: <%= className %>FeatureStore) => {
+        return new Load<%= className %>SuccessAction({
+          <%= camelizedName %>: { id: '1', prop1: 'property' },
+        });
+      },
 
-    onError: (action: LoadDataAction, error) => {
+      onError: (action: Load<%= className %>Action, error) => {
         console.error('Error', error);
+      }
     }
-});
+  );
 
-    constructor(private actions: Actions, private dataPersistence: DataPersistence<<%= className %>State>) {}
+  constructor(
+    private actions: Actions,
+    private dataPersistence: DataPersistence<<%= className %>FeatureStore>,
+  ) {}
 }

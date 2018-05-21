0bd2cb9c4cfa22f39ed2c8027630328d72ad91b6
robt1019 <roberttaylor@tengroup.com>
2018-05-21 12:53:19 +0100

    feat(ngrx): update reducer entity adapter to match new architecture

--- src/ngrx/files/+state/__fileName__.actions.ts
+++ src/ngrx/files/+state/__fileName__.actions.ts
@@ -3,28 +3,28 @@ import { Action } from '@ngrx/store';
 import { <%= className %> } from './<%= fileName %>.reducer'
 
 export enum <%= className %>ActionTypes {
-    LOAD_<%= constName %> = '[<%= className %>] Load <%= fileName %>',
-    LOAD_<%= constName %>_FAILURE = '[<%= className %>] Load <%= fileName %> Failure',
-    LOAD_<%= constName %>_SUCCESS = '[<%= className %>] Load <%= fileName %> Success',
+  LOAD_<%= constName %> = '[<%= className %>] Load <%= fileName %>',
+  LOAD_<%= constName %>_FAILURE = '[<%= className %>] Load <%= fileName %> Failure',
+  LOAD_<%= constName %>_SUCCESS = '[<%= className %>] Load <%= fileName %> Success',
 }
 
 export class Load<%= className %>Action implements Action {
-    public readonly type = <%= className %>ActionTypes.LOAD_<%= constName %>;
+  public readonly type = <%= className %>ActionTypes.LOAD_<%= constName %>;
 }
 
 export class Load<%= className %>FailureAction implements Action {
-    public readonly type = <%= className %>ActionTypes.LOAD_<%= constName %>_FAILURE;
+  public readonly type = <%= className %>ActionTypes.LOAD_<%= constName %>_FAILURE;
 
-    constructor(public payload: { error: any }) {}
+  constructor(public payload: { errors: any }) {}
 }
 
 export class Load<%= className %>SuccessAction implements Action {
-    public readonly type = <%= className %>ActionTypes.LOAD_<%= constName %>_SUCCESS;
+  public readonly type = <%= className %>ActionTypes.LOAD_<%= constName %>_SUCCESS;
 
-    constructor(public payload: { <%= camelizedName %>: <%= className %>[] }) {}
+  constructor(public payload: { <%= camelizedName %>: <%= className %><% if(useEntityAdapter) { %>[]<% } %>:}) {}
 }
 
 export type <%= className %>Action =
-    | Load<%= className %>Action
-    | Load<%= className %>FailureAction
-    | Load<%= className %>SuccessAction;
+  | Load<%= className %>Action
+  | Load<%= className %>FailureAction
+  | Load<%= className %>SuccessAction;
--- src/ngrx/files/+state/__fileName__.reducer.ts
+++ src/ngrx/files/+state/__fileName__.reducer.ts
@@ -1,70 +1,102 @@
 <% if(useEntityAdapter) { %>import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
+<% } %>import { createFeatureSelector, createSelector } from '@ngrx/store';
 
-<% } %>import { <%= className %>Action, <%= className %>ActionTypes } from './<%= fileName %>.actions';
-import { HttpErrorResponse } from '@angular/common/http';
+import { <%= className %>Action, <%= className %>ActionTypes } from './<%= fileName %>.actions';
 <% if(useEntityAdapter) { %>
-// EntityAdapter should be typed based on data being loaded
-const <%= camelizedName %>Adapter: EntityAdapter<any> = createEntityAdapter<any>();
-
+const <%= camelizedName %>Adapter: EntityAdapter<<%= className %>> = createEntityAdapter<<%= className %>>();
 <% } %>
-export interface <%= className %>State <% if(useEntityAdapter) { %> extends EntityState<any><% } %> {<% if(!useEntityAdapter) { %>
-    data: any;<% } %>
-    loading: boolean;
-    loaded: boolean;
-    error: HttpErrorResponse | Error | undefined;
+// Entity type. This should be moved to its own file
+export interface <%= className %> {
+  id: string;
+  prop1: any;
 }
 
-export const initial<%= className %>State = <% if(useEntityAdapter) { %><%= camelizedName %>Adapter.getInitialState( <% } %> {<% if(!useEntityAdapter) { %>
-    data : undefined,<% } %>
-    loading: false,
-    loaded: false,
-    error: undefined,
+// The name under which we register this NgRx feature store
+// inside of this feature's module.ts file.
+export const <%= camelizedName %>FeatureStoreName = '<%= camelizedName %>';
+
+// Highest level interface for this feature store.
+export interface <%= className %>FeatureStore {
+  [<%= camelizedName %>FeatureStoreName]: <%= className %>State;
+}
+
+export interface <%= className %>State <% if(useEntityAdapter) { %> extends EntityState<any><% } %> {<% if(!useEntityAdapter) { %>
+  <%= camelizedName %><% if(useEntityAdapter) { %>[]<% } %>: any;<% } %>
+  loading: boolean;
+  loaded: boolean;
+  errors: any[];
+}
+
+export const initial<%= className %>Data = <% if(useEntityAdapter) { %><%= camelizedName %>Adapter.getInitialState( <% } %> {<% if(!useEntityAdapter) { %>
+  <%= camelizedName %>: undefined,<% } %>
+  loading: false,
+  loaded: false,
+  errors: [],
 }<% if(useEntityAdapter) { %>)<% } %>;
 
-export function <%= camelizedName %>Reducer(state = initial<%= className %>State, action: <%= className %>Action) {
-
-    switch (action.type) {
-
-        case <%= className %>ActionTypes.LOAD_DATA: {
-            return <% if(useEntityAdapter) { %><%= camelizedName %>Adapter.removeAll( <% } %>{
-                ...state,
-                loading: true,
-                loaded: false,
-                error: undefined,
-            }<% if(useEntityAdapter) { %>)<% } %>;
-        }
-
-        case <%= className %>ActionTypes.LOAD_DATA_SUCCESS: {
-            const data = action.payload;
-            return <% if(useEntityAdapter) { %><%= camelizedName %>Adapter.addAll(<%= camelizedName %>, <% } %>{
-                ...state,<% if(!useEntityAdapter) { %>
-                data,<% } %>
-                loading: false,
-                loaded: true,
-                error: undefined,
-            }<% if(useEntityAdapter) { %>)<% } %>;
-        }
-
-        case <%= className %>ActionTypes.LOAD_DATA_FAILURE: {
-            const error = action.payload;
-            return <% if(useEntityAdapter) { %><%= camelizedName %>Adapter.removeAll( <% } %>{
-                ...state,
-                loading: false,
-                loaded: false,
-                error
-            }<% if(useEntityAdapter) { %>)<% } %>;
-        }
-
-        default:
-            return state;
+export function <%= camelizedName %>Reducer(
+  state = initial<%= className %>Data,
+  action: <%= className %>Action,
+): <%= className %>State {
+  switch (action.type) {
+    case <%= className %>ActionTypes.LOAD_<%= constName %>: {
+      return <% if(useEntityAdapter) { %><%= camelizedName %>Adapter.removeAll( <% } %>{
+        ...state,
+        loading: true,
+        errors: [],
+      }<% if(useEntityAdapter) { %>)<% } %>;
     }
+
+    case <%= className %>ActionTypes.LOAD_<%= constName %>_SUCCESS: {
+      return <% if(useEntityAdapter) { %><%= camelizedName %>Adapter.addAll(
+        action.payload.<%= camelizedName %>,
+        <% } %>{
+      ...state,<% if(!useEntityAdapter) { %>
+      <%= camelizedName %>: action.payload.<%= camelizedName %>,<% } %>
+        loading: false,
+        loaded: true,
+        errors: [],
+      }<% if(useEntityAdapter) { %>)<% } %>;
+    }
+
+    case <%= className %>ActionTypes.LOAD_<%= constName %>_FAILURE: {
+      return <% if(useEntityAdapter) { %><%= camelizedName %>Adapter.removeAll( <% } %>{
+        ...state,
+        loading: false,
+        loaded: false,
+        errors: action.payload.errors
+      }<% if(useEntityAdapter) { %>)<% } %>;
+    }
+
+    default:
+      return state;
+  }
 }
+
+/**
+ * Feature store selector
+ */
+export const get<%= className %>FeatureState = createFeatureSelector<<%= className %>State>(
+  <%= camelizedName %>FeatureStoreName,
+);
 <% if(useEntityAdapter) { %>export const {
-    selectAll: selectData
+  selectAll: selectData
 } = <%= camelizedName %>Adapter.getSelectors();
 <% } %>
-<% if(!useEntityAdapter) { %>export const selectData = ((state: <%= className %>State) => state.data);
-<% } %>export const selectDataLoading = ((state: <%= className %>State) => state.loading);
-export const selectDataLoaded = ((state: <%= className %>State) => state.loaded);
-export const selectDataLoadError = ((state: <%= className %>State) => state.error);
+<% if(!useEntityAdapter) { %>export const get<%= className %> = createSelector(
+  get<%= className %>FeatureState,
+  (state: <%= className %>State) => state.<%= camelizedName %>,
+);
+<% } %>export const get<%= className %>LoadingState = createSelector(
+  get<%= className %>FeatureState,
+  (state: <%= className %>State) => state.loading
+);
+export const get<%= className %>LoadedState = createSelector(
+  get<%= className %>FeatureState,
+  (state: <%= className %>State) => state.loaded
+);
+export const get<%= className %>LoadError = createSelector(
+  get<%= className %>FeatureState,
+  (state: <%= className %>State) => state.errors
+);
 
